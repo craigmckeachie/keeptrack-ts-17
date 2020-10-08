a04dcfafcefd1b30d4e0f94aca90953ec0f2f564
Craig McKeachie <craigdmckeachie@gmail.com>
2020-10-08 14:54:22 -0400

    reverting back to the beginning of lab14

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -1,63 +1,27 @@
-import React, { SyntheticEvent, useState } from 'react';
+import React, { SyntheticEvent } from 'react';
 import { Project } from './Project';
 
 interface ProjectFormProps {
-  project: Project;
   onSave: (project: Project) => void;
   onCancel: () => void;
 }
 
-function ProjectForm({ project, onSave, onCancel }: ProjectFormProps) {
-  const [name, setName] = useState(project.name);
-  const [description, setDescription] = useState(project.description);
-  const [budget, setBudget] = useState(project.budget);
-  const [isActive, setIsActive] = useState(project.isActive);
-
+function ProjectForm({ onSave, onCancel }: ProjectFormProps) {
   const handleSubmit = (event: SyntheticEvent) => {
     event.preventDefault();
-    onSave(new Project({ ...project, name, description, budget, isActive }));
+    onSave(new Project({ name: 'Updated Project' }));
   };
 
   return (
     <form className="input-group vertical" onSubmit={handleSubmit}>
       <label htmlFor="name">Project Name</label>
-      <input
-        type="text"
-        name="name"
-        placeholder="enter name"
-        value={name}
-        onChange={(event) => {
-          setName(event.target.value);
-        }}
-      />
+      <input type="text" name="name" placeholder="enter name" />
       <label htmlFor="description">Project Description</label>
-      <textarea
-        name="description"
-        placeholder="enter description"
-        value={description}
-        onChange={(event) => {
-          setDescription(event.target.value);
-        }}
-      />
+      <textarea name="description" placeholder="enter description" />
       <label htmlFor="budget">Project Budget</label>
-      <input
-        type="number"
-        name="budget"
-        placeholder="enter budget"
-        value={budget}
-        onChange={(event) => {
-          setBudget(Number(event.target.value));
-        }}
-      />
+      <input type="number" name="budget" placeholder="enter budget" />
       <label htmlFor="isActive">Active?</label>
-      <input
-        type="checkbox"
-        name="isActive"
-        checked={isActive}
-        onChange={(event) => {
-          setIsActive(Boolean(event.target.checked));
-        }}
-      />
+      <input type="checkbox" name="isActive" />
       <div className="input-group">
         <button className="primary bordered medium">Save</button>
         <span />
