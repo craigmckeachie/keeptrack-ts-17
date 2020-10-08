7260bb689fe3f9eccf78a7ab6d2422940f4c4365
Craig McKeachie <craigdmckeachie@gmail.com>
2020-10-08 17:25:08 -0400

    fix so at lab14

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,21 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { MOCK_PROJECTS } from './MockProjects';
 import { Project } from './Project';
 import ProjectList from './ProjectList';
 
 function ProjectsPage() {
-  const [projects, setProjects] = useState(MOCK_PROJECTS);
   const saveProject = (project: Project) => {
-    let updatedProjects = projects.map((p: Project) => {
-      return p.id === project.id ? project : p;
-    });
-    setProjects(updatedProjects);
+    console.log('Saving project: ', project);
   };
 
   return (
     <>
       <h1>Projects</h1>
-      <ProjectList projects={projects} onSave={saveProject} />
+      <ProjectList projects={MOCK_PROJECTS} onSave={saveProject} />
     </>
   );
 }
