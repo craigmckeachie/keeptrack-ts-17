391521f767fd1ccd19c7b46a932b0d3e3b3d9d3a
Craig McKeachie <craigdmckeachie@gmail.com>
2020-10-09 09:32:52 -0400

    lab20

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from 'react-router-dom';
 
 import './App.css';
+import HomePage from './home/HomePage';
 import ProjectsPage from './projects/ProjectsPage';
 
 function App() {
-  return <ProjectsPage />;
+  return (
+    <Router>
+      <header className="sticky">
+        <span className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+        </span>
+        <NavLink to="/" exact className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects/" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+      <div className="container">
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/projects" exact component={ProjectsPage} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
