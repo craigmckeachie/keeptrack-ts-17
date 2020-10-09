645dc657a25e4da4f7eca7cb86f6d4726f8a56ce
Craig McKeachie <craigdmckeachie@gmail.com>
2020-10-09 15:16:05 -0400

    lab25

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,30 +11,33 @@ import './App.css';
 import HomePage from './home/HomePage';
 import ProjectPage from './projects/ProjectPage';
 import ProjectsPage from './projects/ProjectsPage';
+import { store } from './state';
 
 function App() {
   return (
-    <Router>
-      <header className="sticky">
-        <span className="logo">
-          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
-        </span>
-        <NavLink to="/" exact className="button rounded">
-          <span className="icon-home"></span>
-          Home
-        </NavLink>
-        <NavLink to="/projects/" className="button rounded">
-          Projects
-        </NavLink>
-      </header>
-      <div className="container">
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/projects" exact component={ProjectsPage} />
-          <Route path="/projects/:id" component={ProjectPage} />
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <header className="sticky">
+          <span className="logo">
+            <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+          </span>
+          <NavLink to="/" exact className="button rounded">
+            <span className="icon-home"></span>
+            Home
+          </NavLink>
+          <NavLink to="/projects/" className="button rounded">
+            Projects
+          </NavLink>
+        </header>
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/projects" exact component={ProjectsPage} />
+            <Route path="/projects/:id" component={ProjectPage} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,49 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import { Project } from './Project';
-import { projectAPI } from './projectAPI';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { AppState } from '../state';
 import ProjectList from './ProjectList';
+import { loadProjects } from './state/projectActions';
 
 function ProjectsPage() {
-  const [projects, setProjects] = useState<Project[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(undefined);
-  const [currentPage, setCurrentPage] = useState(1);
+  const loading = useSelector(
+    (appState: AppState) => appState.projectState.loading
+  );
+  const projects = useSelector(
+    (appState: AppState) => appState.projectState.projects
+  );
+  const error = useSelector(
+    (appState: AppState) => appState.projectState.error
+  );
+  const currentPage = useSelector(
+    (appState: AppState) => appState.projectState.page
+  );
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    async function loadProjects() {
-      setLoading(true);
-      try {
-        const data = await projectAPI.get(currentPage);
-        if (currentPage === 1) {
-          setProjects(data);
-        } else {
-          setProjects((projects) => [...projects, ...data]);
-        }
-      } catch (e) {
-        setError(e.message);
-      } finally {
-        setLoading(false);
-      }
-    }
-    loadProjects();
-  }, [currentPage]);
-
-  const saveProject = (project: Project) => {
-    projectAPI
-      .put(project)
-      .then((updatedProject) => {
-        let updatedProjects = projects.map((p: Project) => {
-          return p.id === project.id ? project : p;
-        });
-        setProjects(updatedProjects);
-      })
-      .catch((e) => {
-        setError(e.message);
-      });
-  };
+    dispatch(loadProjects(1));
+  }, [dispatch]);
 
   const handleMoreClick = () => {
-    setCurrentPage((currentPage) => currentPage + 1);
+    dispatch(loadProjects(currentPage + 1));
   };
 
   return (
@@ -63,8 +44,7 @@ function ProjectsPage() {
         </div>
       )}
 
-      <ProjectList projects={projects} onSave={saveProject} />
-
+      <ProjectList projects={projects} />
       {!loading && !error && (
         <div className="row">
           <div className="col-sm-12">
